3f5f60b4e015955a2aff782a760083273aeb5226
astral <astral@astral.cafe>
2025-06-02 17:24:19 -0600

    Quartz sync: Jun 2, 2025, 5:24 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Astral1119/sheets-wiki-data",
+      "Discord Community": "https://discord.gg/M9GKpPd",
+      "Subreddit": "https://reddit.com/r/sheets",
     },
   }),
 }
